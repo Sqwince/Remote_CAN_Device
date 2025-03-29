0f3840dc08869b34eb79c8ab93b1becabbc6fe92
Sqwince <sqwince@aol.com>
2025-03-29 13:09:39 -0500

    Reupload RGB effects with Renamed RCD - tested working

--- Remote_CAN_Device/Config.h
+++ Remote_CAN_Device/Config.h
@@ -1,6 +1,39 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/*#############################################################*/
+/*#######          OpenFFBoard Pin Definitions          #######*/
+/*#############################################################*/
+//https://github.com/Ultrawipf/OpenFFBoard/wiki/Pinouts-and-peripherals)
+
+//Onboard LEDs
+#define LED_BLU_Pin PD7  //Blue   (Sys)
+#define LED_RED_Pin PE1  //Red    (Error)
+#define LED_YEL_Pin PE0  //Yellow (Clip)
+
+/*Digital Inputs*/
+#define DIN_1 PC15
+#define DIN_2 PC14
+#define DIN_3 PC13
+#define DIN_4 PE6
+#define DIN_5 PE5
+#define DIN_6 PE4
+#define DIN_7 PE3
+#define DIN_8 PE2
+
+/*Analog Inputs*/
+#define AIN_1 PA3
+#define AIN_2 PA2
+#define AIN_3 PC3
+#define AIN_4 PC2
+#define AIN_5 PC1
+#define AIN_6 PC0
+
+/* SPI-2 Interface */
+#define SPI2_SCK_PIN PB13   //Clock
+#define SPI2_MISO_PIN PB14  //Data In from last shift register in chain
+#define SPI2_MOSI_PIN PB15  //Data out for driving RGB LED String (WS2812B)
+#define SPI2_CS_PIN PB12    //Data Latch
 
 
 /*#########################################*/
@@ -13,25 +46,7 @@ enum FrameIndexNumber {
 };
 
 //TODO: Convert this to enum?
-uint32_t OpenFFB_Analog_Input_Pins[6] = {
-  PA3,  //OpenFFB Analog Input 1
-  PA2,  //OpenFFB Analog Input 2
-  PC3,  //OpenFFB Analog Input 3
-  PC2,  //OpenFFB Analog Input 4
-  PC1,  //OpenFFB Analog Input 5
-  PC0   //OpenFFB Analog Input 6
-};      //Ref:https://github.com/Ultrawipf/OpenFFBoard/wiki/Pinouts-and-peripherals)
-
-
-//OpenFFBoard Digital Input Pins
-#define DIN_1 PC15
-#define DIN_2 PC14
-#define DIN_3 PC13
-#define DIN_4 PE6
-#define DIN_5 PE5
-#define DIN_6 PE4
-#define DIN_7 PE3
-#define DIN_8 PE2
+uint32_t OpenFFB_Analog_Input_Pins[6] = {AIN_1,AIN_2,AIN_3,AIN_4,AIN_5,AIN_6};      
 
 
 
@@ -61,24 +76,20 @@ uint32_t analogPins[ENABLED_ANALOG_AXIS_NUM];  //array of pin#s for enabled anal
 #define SPI_BUTTON_BOARDS 1  //Number of SPI-Button boards used (0-2)
 bool DIGITAL_INPUTS_ENABLED = false;
 
-//---  Openffbaord SPI2 Interface Pins Definition  ---
-#define SPI2_SCK_PIN PB13   //Clock
-#define SPI2_MISO_PIN PB14  //Data In from last shift register in chain
-#define SPI2_CS_PIN PB12    //Data Latch
+
 
 /*  ROTARY ENCODER KNOB SETTINGS
     A/B encoder phases attached to interrupts
-    converted into a pulse lasting (default = 100ms) for HID to read. 
+    converted into a pulse lasting for HID to read. 
     Encoder Update Returns: -1 for CCW direction 
     Encoder Update Returns: +1 for CW direction
-    Encoder Update Returns:  0 once pulse has timed out
-*/
-#define ROTARY_ENCODER_COUNT 2  //Number of Rotary Encoder knobs used (0-4)
-#define ENCODER_PULSE_MS 50     //Encoder Pulse length in milliseconds
+    Encoder Update Returns:  0 once pulse has timed out*/
+#define ROTARY_ENCODER_COUNT 2  //Number of Rotary Encoder knobs used (Supports up to 4)
+#define ENCODER_PULSE_MS 50     //Encoder Pulse length in milliseconds (default = 50ms)
 
 /*  ROTARY ENCODER KNOB #1 PINS */
-#define ENC1_A_PIN DIN_4
-#define ENC1_B_PIN DIN_2
+#define ENC1_A_PIN DIN_2
+#define ENC1_B_PIN DIN_4
 
 /*  ROTARY ENCODER KNOB #2 PINS */
 #define ENC2_A_PIN DIN_6
@@ -96,16 +107,21 @@ bool DIGITAL_INPUTS_ENABLED = false;
 /*########################################################*/
 /*#######   General Settings & Timing Parameters   #######*/
 /*########################################################*/
+
 #define POLLING_FREQUENCY 500  //Input polling frequency [Default: 120Hz]
 #define CAN_SPEED 500000       //500 kbps for OpenFFBoard
 #define CAN_TXMSG_SIZE 8       //8 Bytes
 #define DEBUG_ENABLED false    //serial print Debug msgs
 #define DEBUG_CAN_MSGS false   //Serial print DATA sent over CAN
 
-//OpenFFB onboard LEDs
-#define LED_BLU_Pin PD7  //Blue   (Sys)
-#define LED_RED_Pin PE1  //Red    (Error)
-#define LED_YEL_Pin PE0  //Yellow (Clip)
+/* RGB LED Strip Settings */
+#define NUM_LEDS 18         //Total count of LEDs for strip
+#define LED_DATA_PIN PB15  //WS2812B Data Input
+#define LED_REFRESH_RATE 20     //RefreshRate [10 FPS]
+
+/** CAUTION: Be mindful of LED current draw when setting MAX brightness **/
+#define MAX_BRIGHTNESS 20   //maximum brightness [0-255]     
+
 
 uint8_t CAN_Frame_interval = 8;    //Time in ms between sending CAN Frames
 uint8_t FrameCount = 0;            //Number of CAN Frames enabled
